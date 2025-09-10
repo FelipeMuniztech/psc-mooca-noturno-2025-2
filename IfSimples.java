0d9e6697dfbbc977675060d073b9070b0d9e4998
FelipeMuniztech <825154362@ulife.com.br>
2025-09-10 00:25:22 -0300

    Trabalho estrutura de decisão com If

--- IfSimples.java
+++ IfSimples.java
@@ -1,5 +1,5 @@
 import javax.swing.JOptionPane;
-
+ 
 public class IfSimples{
     public static void main(String[] args) {
         int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número"));
