a08becb443794d16262c516ddfbe7dbe8ddb623d
FelipeMuniztech <825154362@ulife.com.br>
2025-09-10 00:16:41 -0300

    Update While.java

--- While.java
+++ While.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DoWhile {
+public class While {
  public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int numero;
